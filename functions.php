2a5de772ab04d760e76cd7e04a007ec3b445faa1
Jota <jmac@Jotas-Mac-mini.local>
2019-05-10 13:23:41 -0400

    extra code removed

--- functions.php
+++ functions.php
@@ -1,53 +1,5 @@
 <?php 
 
-function parent_css_theme_style() { 
- wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' ); 
-}
-add_action( 'wp_enqueue_scripts', 'parent_css_theme_style' );
-
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page(array(
-		'page_title' 	=> 'iEatWebsites General Settings',
-		'menu_title'	=> 'ieatSettings',
-		'menu_slug' 	=> 'theme-general-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Header Settings',
-		'menu_title'	=> 'Header',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-	
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
-		'menu_title'	=> 'Footer',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
-		'menu_title'	=> 'Style',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
-		'menu_title'	=> 'Whatever',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Footer Settings',
-		'menu_title'	=> 'Something Else',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	
-}
-
 function register_acf_block_types() {
 
     // register block.
